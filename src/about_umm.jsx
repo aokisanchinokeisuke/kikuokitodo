e87fbb562aa00542a5187c51ff91b4506b2b5c85
yuuto52925goal <yuuto52925goal@gmail.com>
2024-04-06 18:32:21 -0600

    change

---
+++ src/about_umm.jsx
@@ -0,0 +1,23 @@
+// import { memo } from 'react';
+// import type { FC } from 'react';
+
+// import resets from '../../_resets.module.css';
+// import classes from './Component43.module.css';
+
+// interface Props {
+//   className?: string;
+//   classes?: {
+//     root?: string;
+//   };
+// }
+/* @figmaId 21:16128 */
+export const Component43 = ()=> {
+  return (
+    <div>
+      <div ></div>
+      <h1>Hi</h1>
+      <div >-h</div>
+      <div >Time</div>
+    </div>
+  );
+};
